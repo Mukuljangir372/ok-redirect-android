debc8549a67f5799c4e50e56638aa02f738a6637
mukuljangir372 <mukul.jangir372@gmail.com>
2023-02-16 18:16:12 +0530

    okredirect doc cm

--- sdk/src/main/java/com/ok/redirect/sdk/OkRedirect.kt
+++ sdk/src/main/java/com/ok/redirect/sdk/OkRedirect.kt
@@ -4,6 +4,19 @@ import android.content.Context
 import android.content.Intent
 import com.ok.redirect.sdk.source.RedirectSourceType
 
+/**
+ * To Use OkRedirect -
+ * OkRedirect.getInstance().open(context, type)
+ *
+ * PRECAUTIONS -
+ * Before using OkRedirect -
+ * 1. Enable view/data binding feature in your project
+ * - build.gradle.app :
+ *     buildFeatures {
+ *        dataBinding true
+ *        viewBinding true
+ *    }
+ */
 class OkRedirect {
     companion object {
         private var instance: OkRedirect? = null
